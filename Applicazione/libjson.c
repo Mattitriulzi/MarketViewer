e6f6f575298c025082663983c67754e46ef4f0c2
mattitriulzi <mmtriulzi@gmail.com>
2024-05-23 16:56:06 +0200

    The jansson library was not correctly implemented, meaning the reading of the json file was completely wrong. Created an interface.cpp file which will allow for a C++ interface. Fixed a few forgotten parenthesis etc

--- Applicazione/libjson.c
+++ Applicazione/libjson.c
@@ -25,7 +25,6 @@ int json(void)
     if (!root_active || !root_news)
     {
         perror("Unable to load json string");
-        fprintf(stderr, "%d : %s", error.line, error.text);
         return 7;
     }
     free(json_active);
@@ -39,21 +38,21 @@ int json(void)
         json_decref(root_news);
         return 8;
     }
-
+    json_t *last_updated_json = json_object_get(root_active, "last_updated");
     // Get the date so that we are able to avoid duplications
-    const char* last_updated = json_string_value(json_get_object(root_active, "last_updated"));
-    if (!(json_is_string(last_updated)))
+    if (!(json_is_string(last_updated_json)))
     {
         perror("Error when reading json file");
         return 8;
     }
+    const char* last_updated = json_string_value(last_updated_json);
     /*Read the last_updated string to be able to check for the first space and keep
     only the date rather than the whole string, this is the format:
     "last_updated": "2024-05-22 16:15:59 US/Eastern"
     We wish to keep only the date. However we have to first copy the string into another 
     variable as this one is a constant*/
     date = strdup(last_updated);
-    for (int i = 0; i < len(date); i++)
+    for (int i = 0; i < strlen(date); i++)
     {
         if (*(date + i) == ' ')
         {
@@ -61,7 +60,7 @@ int json(void)
             break;
         }
     }
-    free(last_updated);
+    free(last_updated_json);
 
 
 
@@ -88,61 +87,69 @@ int json(void)
         and the original object*/
         const char *ticker, *price, *price_change, *change_percentage, *volume;
 
-
+        json_t *ticker_json = json_object_get(most_active, "ticker");
+        if (!json_is_string((ticker_json)))
+        {
+            perror("Error when reading object array");
+            json_decref(most_active);
+            json_decref(root_active);
+            return 10;
+        }
         ticker = json_string_value(json_object_get(most_active, "ticker"));
-        if (!json_is_string((ticker))
-        {
-            perror("Error when reading object array");
-            json_decref(most_active);
-            json_decref(root_active);
-            free(ticker);
-            return 10;
-        }
         (active_stocks + i)->ticker = strdup(ticker);
+        json_decref(ticker_json);
 
 
-        price = json_string_value(json_object_get(most_active, "price"));
-        if (!json_is_string(price))
+        json_t *price_json = json_object_get(most_active, "price");
+        if (!json_is_string(price_json))
         {
             perror("Error when reading object array");
             json_decref(most_active);
             json_decref(root_active);
             return 10;
         }
+        price = json_string_value(price_json);
         (active_stocks + i)->price = strdup(price);
+        json_decref(price_json);
 
 
-        price_change = json_string_value(json_object_get(most_active, "change_amount"));
-        if (!json_is_string(price_change))
+        json_t *price_change_json = json_object_get(most_active, "change_amount");
+        if (!json_is_string(price_change_json))
         {
             perror("Error when reading object array");
             json_decref(most_active);
             json_decref(root_active);
             return 10;
         }
-        (active_stocks + i)->price_change = strdup(price_change)
+        price_change = json_string_value(price_change_json);
+        (active_stocks + i)->price_change = strdup(price_change);
+        json_decref(price_change_json);
 
 
-        change_percentage = json_string_value(json_object_get(most_active, "change_percentage"))
-        if (!json_is_string(change_percentage)
+        json_t *change_percentage_json = json_object_get(most_active, "change_percentage");
+        if (!json_is_string(change_percentage_json))
         {
             perror("Error when reading object array");
             json_decref(most_active);
             json_decref(root_active);
             return 10;
         }
+        change_percentage = json_string_value(change_percentage_json);
         (active_stocks + i)->change_percentage = strdup(change_percentage);
+        json_decref(change_percentage_json);
 
 
-        volume = json_string_value(json_object_get(most_active, "volume"));
-        if (!json_is_string(volume))
+        json_t *volume_json = json_object_get(most_active, "volume");
+        if (!json_is_string(volume_json))
         {
             perror("Error when reading object array");
             json_decref(most_active);
             json_decref(root_active);
             return 10;
         }
+        volume = json_string_value(volume_json);
         (active_stocks + i)->volume = strdup(volume);
+        json_decref(volume_json);
     }
     /*freeing all the variables used until now, leaving only the struct with all the values needed*/
     free(ticker);
@@ -175,69 +182,91 @@ int json(void)
 
     for (int i = 0; i < LENGTH_NEWS; i++)
     {
-        char *title, *url, *summary, *sentiment, *tickers;
+        const char *title, *url, *summary, *sentiment; // tickers is a special case, hence will initialise later
 
-
-        title = json_string_value(json_object_get(feed, "title"));
-        if (!json_is_string(title))
+        json_t *title_json = json_object_get(feed, "title");
+        if (!json_is_string(title_json))
         {
             perror("Error when reading object array");
             json_decref(feed);
             json_decref(root_news);
             return 11;
         }
+        title = json_string_value(title_json);
         (sentiments + i)->title = strdup(title);
+        json_decref(title_json);
 
 
-        url = json_string_value(json_object_get(feed, "url"));
-        if(!json_is_string(url))
+        json_t *url_json = json_object_get(feed, "url");
+        if(!json_is_string(url_json))
         {
             perror("Error when reading object array");
             json_decref(feed);
             json_decref(root_news);
             return 11;
         }
-        (sentiments + i)->url = strdup(url);
+        url = json_string_value(url_json);
+        (sentiments + i)->URL = strdup(url);
+        json_decref(url_json);
 
 
-        summary = json_string_value(json_object_get(feed,"summary"));
-        if(!json_is_string(summary))
+        json_t *summary_json = json_object_get(feed, "summary");
+        if(!json_is_string(summary_json))
         {
             perror("Error when reading object array");
             json_decref(feed);
             json_decref(root_news);
             return 11;
         }
-        (sentiments + i)->summary = strdup(summary)
+        summary = json_string_value(summary_json);
+        (sentiments + i)->summary = strdup(summary);
+        json_decref(summary_json);
 
 
-        sentiment = json_string_value(json_object_get(feed, "overall_sentiment_label"));
-        if(!json_is_string(sentiment))
+        json_t *sentiment_json = json_object_get(feed, "overall_sentiment_label");
+        if(!json_is_string(sentiment_json))
         {
             perror("Error when reading object array");
             json_decref(feed);
             json_decref(root_news);
             return 11;
         }
+        sentiment = json_string_value(sentiment_json);
         (sentiments + i)->sentiment = strdup(sentiment);
+        json_decref(sentiment_json);
 
-        json_t *tickers_array = json_get_array(feed, "tickers_sentiment")
+
+        json_t *tickers_array = json_array_get(feed, 13);
         if (!json_is_array(tickers_array))
         {
             perror("Error when reading object array");
-            jeson_decref(feed);
+            json_decref(feed);
             json_decref(root_news);
             return 12;
         }
 
 
-        tickers = (char *) calloc(8, sizeof(char)); // initially give it a size of 8 as tickers cannot have more than 5
-        // + coma + space + null terminator letters
+        char *tickers = (char *) calloc(12, sizeof(char)); /*initially give it a size of 12 as tickers cannot have more than 5
+         + coma + space + null terminator letters P.S. Had to add possibility for CRYPTO:BTC */
         for (int j = 0; j < json_array_size(tickers_array); j++)
         {
+
+            json_t *current_ticker = json_array_get(tickers_array, j);
+            if (!json_is_array(current_ticker))
+            {
+                perror("Error when readng object array");
+                json_decref(feed);
+                json_decref(root_news);
+                json_decref(current_ticker);
+                return 13;
+            }
+            
+
+
+
             // create temporary ticker that will be added to the tickers char to have a long char with all possible tickers
-            char *temp_ticker = json_string_value(json_get_object(tickers_array, "ticker"));
-            if (!json_is_string(temp_ticker))
+            json_t *temp_ticker_json = json_object_get(tickers_array, "ticker");
+            if (!json_is_string(temp_ticker_json))
             {
                 perror("Error when reading object array");
                 json_decref(tickers_array);
@@ -245,18 +274,24 @@ int json(void)
                 json_decref(root_news);
                 return 12;
             }
-            strcat(temp_ticker, ", ");
-            if (!j)
-                strcpy(tickers, temp_tickers);
+            const char *temp_ticker = json_string_value(temp_ticker_json);
+
+
+
+
+            if (!j){
+                strcpy(tickers, temp_ticker);
+                strcat(tickers, ", ");
+            }
             else
             {
-                tickers = realloc(tickers, (strlen(tickers) + strlen(temp_ticker)) * sizeof(char) );
+                tickers = realloc(tickers, (strlen(tickers) + strlen(temp_ticker)) * sizeof(char) + 2 );
                 if (tickers == NULL)
                 {
                     perror("Unable to allocate memory");
                     return 12;
                 }
-
+                
                 strcat(tickers, temp_ticker); 
             } 
             (sentiments + i)->tickers = strdup(tickers);
@@ -302,8 +337,9 @@ char *read_file(FILE *file, int length)
     }
     int n = 0; // number of iterations
 
-    if(!(char *buffer = (char *) calloc()) {
-        perror("Unable to allocate")
+    if(!(char *buffer = (char *) calloc()))
+    {
+        perror("Unable to allocate");
     }
 
     size_t read;
