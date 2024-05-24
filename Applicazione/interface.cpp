d6e69f714ee2f85b77992abd081c0bed3f538c64
Mattia <144261950+Mattitriulzi@users.noreply.github.com>
2024-05-24 11:04:37 +0200

    initial test for QT GUI

--- Applicazione/interface.cpp
+++ Applicazione/interface.cpp
@@ -1,3 +1,15 @@
+
 #include <iostream>
-#include "stock.h"
 #include <QApplication>
+#include <QmainWindow>
+
+int main()
+{
+    int argc = 1;
+    char *argv[] = {"Test", NULL };
+    QApplication app(argc,argv);
+    QMainWindow mainwindow;
+    mainwindow.show();
+
+    return app.exec();
+}
