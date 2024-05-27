a36a3c4b2e64f902b6775657c1882f921c62f793
Mattia <mmtriulzi@gmail.com>
2024-05-27 20:19:44 +0200

    Debugged the SQL_read part, it used to try to sel ect the date even when the table was empty, leading to an error

--- Applicazione/SQL_read.c
+++ Applicazione/SQL_read.c
@@ -27,20 +27,29 @@ typedef struct news
 // if true, then the app was already started today and no need to print into the database
 bool already_opened = 0; 
 
-int callback(void *p, int argc, char **argv, char**azColName);
+int callback_empty(void *count, int argc, char **argv, char** azColName);
+
+int callback(void *p, int argc, char **argv, char **azColName);
+
+int is_table_empty(sqlite3 *db, const char *table_name);
+
 int SQL_read(void)
 {
     char *errmsg = NULL;
     const char *table_active = "Most_active";
     const char *table_news = "News";
 
-    // check the date of the last entry to avoid dup entries
-    int rd = sqlite3_exec(db, "SELECT date FROM table_active LIMIT 1 ", callback, 0, &errmsg);
-    if (rd)
+    
+    if (!is_table_empty(db, table_active))
     {
+        int rd = sqlite3_exec(db, "SELECT date FROM Most_active LIMIT 1 ", callback, 0, &errmsg);
+        if (rd)
+        {
         perror("Unable to access database");
         return 400;
+        }
     }
+    // check the date of the last entry to avoid dup entries
     if(!already_opened) goto Stock_read;
     return 0;
 
@@ -95,5 +104,30 @@ int callback(void *p, int argc, char** argv, char** azColName)
         if(!strcmp(*(azColName + date_position), "date")) break;
     }
     if (!strcmp(*(argv + date_position), date)) already_opened = 1;
+    return 0;
+}
+
+
+int is_table_empty(sqlite3 *db, const char *table_name)
+{
+    // function that will check whether a table is empty
+    char *errmsg = NULL;
+    int count = 0;
+    char *command = sqlite3_mprintf("SELECT Count (*) FROM '%q", table_name);
+    int rc = sqlite3_exec(db, command, callback_empty, &count, &errmsg);
+    if (rc != SQLITE_OK)
+    {
+        perror("cannot execut demand");
+        return -1;
+    }
+    if (!count) return 1;
+    return 0;
+}
+
+
+
+int callback_empty(void *count, int argc, char **argv, char **azColName)
+{
+    *(int *)count = atoi(argv[0]);
     return 0;
 }
