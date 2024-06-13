6eae29783c71273fa1d5adb4f706f56a12248f35
Mattia <144261950+Mattitriulzi@users.noreply.github.com>
2024-06-13 16:40:09 +0200

    Full Debug
    Release now has a full logger that logs almost every operation, file deletion, optimised functions and no memory leaks.
    
    A known issue is the set fault that happens about 1/15 when using QTCore, fix incoming

--- app/json.c
+++ app/json.c
@@ -33,13 +33,13 @@ int json(void)
     log_it("Successfully loaded the json files, closing files");
     fclose(stock_data_active);
     fclose(stock_data_sentiment);
-    /*int error_return = fdelete(filename, arraylen);
+    int error_return = fdelete(filename, arraylen);
     if (error_return){
         perror("Unable to delete file");
         log_it("Unable to delete file");
         return 301;
     }
-    log_it("Successfully deleted the temporary files"); */
+    log_it("Successfully deleted the temporary files"); 
     
 
     log_it("Successfully closed temporary files");
